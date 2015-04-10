c8c31a6425e46c06a9b6430ea2dbedc1df337894
Richard Elms <richardelms@gmail.com>
2015-04-10 23:10:02 +0200

    changed movement to lerp, seems much smoother

--- Assets/Scripts/Util/MoveSectionsRight.cs
+++ Assets/Scripts/Util/MoveSectionsRight.cs
@@ -2,16 +2,16 @@
 using System.Collections;
 
 public class MoveSectionsRight : MonoBehaviour {
-
+	
 	void Update()
 	{
-		transform.position = new Vector3(
-			transform.position.x + (GameController.gameSpeed * Time.deltaTime),
+		Vector3 target = new Vector3(
+			transform.position.x + (GameController.gameSpeed * Time.smoothDeltaTime),
 			transform.position.y,
 			transform.position.z
 			);
 		
-		
+		transform.position = Vector3.Lerp (transform.position,target,1);
 		
 	}
 }
