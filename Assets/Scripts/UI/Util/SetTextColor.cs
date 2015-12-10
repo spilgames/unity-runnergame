6730daae12072214d474773558c2be1dcddd0df8
Richard Elms <richardelms@gmail.com>
2015-12-10 20:12:56 +0100

    bug fixes

---
+++ Assets/Scripts/UI/Util/SetTextColor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class SetTextColor : MonoBehaviour {
+
+	public Color w1,w2,w3;
+
+	// Use this for initialization
+	void Start () {
+		if(Application.loadedLevelName.Contains("W01")){
+			GetComponent<Text>().color = w1;
+		}
+		if(Application.loadedLevelName.Contains("W02")){
+			GetComponent<Text>().color = w2;
+		}
+		if(Application.loadedLevelName.Contains("W03")){
+			GetComponent<Text>().color = w3;
+		}
+	}
+	
+
+}
